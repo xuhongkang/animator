93ee1796be3c5bfde4daf57f8d04e6148d385533
Kane Xu <kanexu.work@gmail.com>
2022-05-05 16:25:21 -0400

    Delete Part1 directory

--- Part1/ProjectFiles/src/cs3500/animator/controller/commands/AnimatorCommand.java
+++
@@ -1,9 +0,0 @@
-package cs3500.animator.controller.commands;
-
-/**
- * For representing the kinds of commands the controller will receive.
- */
-public interface AnimatorCommand {
-  void build();
-}
-
